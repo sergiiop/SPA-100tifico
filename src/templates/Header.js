967a04703e53307e4bcdaa713f62d62d1198ccbc
Sergio Pérez <sergio-luis98@hotmail.com>
2022-09-10 01:05:45 -0500

    fix/header link

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div class="header-main">
       <div class="header-logo">
         <h1>
-          <a href="/SPA-100tifico">
+          <a href="#/">
             100tifi.co
           </a>
         </h1>
